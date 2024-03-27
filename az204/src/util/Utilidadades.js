588a0877dc1c67dd4c10446a14ff079230dd5de0
William Farinas <wgu70804@gmail.com>
2024-03-27 16:30:37 -0600

    contador de segundos

--- az204/src/util/Utilidadades.js
+++ az204/src/util/Utilidadades.js
@@ -1,8 +1,25 @@
 export const ObtenerTiempo = (segundosP) => {
-    const segundos = (Math.round(segundosP % 0x3C)).toString();
-    const horas    = (Math.floor(segundosP / 0xE10)).toString();
-    const minutos  = (Math.floor(segundosP / 0x3C ) % 0x3C).toString();
+    const valores = [];
 
-    return `${horas} horas, ${minutos} minutos y ${segundos} segundos.`;
+    const segundos = (Math.round(segundosP % 0x3C));
+    const minutos = (Math.floor(segundosP / 0x3C) % 0x3C);
+    const horas = (Math.floor(segundosP / 0xE10));
+
+    console.log(horas)
+    if (horas) {
+        valores.push(horas + " horas");
+    }
+
+    if (minutos) {
+        valores.push(minutos + " minutos");
+    }
+
+    valores.push(segundos + " segundos");
+
+    const formatter = new Intl.ListFormat('es', {
+        style: 'long',
+        type: 'conjunction',
+    });
+    return formatter.format(valores)
 
 }
