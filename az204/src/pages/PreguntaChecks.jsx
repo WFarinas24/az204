7f124bcef1ea796658a3eb3555a10b1b4a08d3d8
wfarinas <wgu70804@gmail.com>
2024-10-11 00:51:39 -0600

    dual box

--- az204/src/pages/PreguntaChecks.jsx
+++ az204/src/pages/PreguntaChecks.jsx
@@ -1,82 +1,46 @@
-import * as React from "react";
-import { useMemo, useState } from "react";
-import * as ReactDOM from "react-dom";
-
+import React, { useState } from "react";
+import DualListBox from "react-dual-listbox";
+import "react-dual-listbox/lib/react-dual-listbox.css";
 import {
-  DndContext,
-  useDraggable,
-  useDroppable,
-} from "@dnd-kit/core";
-import { CSS } from "@dnd-kit/utilities";
+    MdOutlineDoubleArrow,
+  MdOutlineKeyboardArrowDown,
+  MdOutlineKeyboardArrowLeft,
+  MdOutlineKeyboardArrowRight,
+  MdOutlineKeyboardArrowUp,
+  MdOutlineKeyboardDoubleArrowDown,
+  MdOutlineKeyboardDoubleArrowLeft,
+  MdOutlineKeyboardDoubleArrowRight,
+  MdOutlineKeyboardDoubleArrowUp,
+} from "react-icons/md";
 
+const options = [
+  { value: "one", label: "Option One" },
+  { value: "two", label: "Option Two" },
+];
 export const PreguntaChecks = () => {
-  const containers = ["A", "B", "C"];
-  const [parent, setParent] = useState(null);
-
-  const item = <Draggable id={ "draggable-item"} />;
+  const [selected, setSelected] = useState([]);
 
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      {parent === null ? item : null}
-
-      <div style={{ display: "flex" }}>
-        {containers.map((id) => (
-          <Droppable key={id} id={id}>
-            {parent === id ? item : "Drop here"}
-          </Droppable>
-        ))}
-      </div>
-    </DndContext>
+    <DualListBox
+      icons={{
+        moveToAvailable: <MdOutlineKeyboardArrowLeft />,
+        moveAllToAvailable: [
+          <MdOutlineKeyboardDoubleArrowLeft key={1} />,
+        ],
+        moveToSelected: <MdOutlineKeyboardArrowRight />,
+        moveAllToSelected: [
+            <MdOutlineKeyboardDoubleArrowRight key={1} />,
+        ],
+        moveDown: <MdOutlineKeyboardArrowDown />,
+        moveUp: <MdOutlineKeyboardArrowUp />,
+        moveTop: <MdOutlineKeyboardDoubleArrowUp />,
+        moveBottom: <MdOutlineKeyboardDoubleArrowDown />,
+      }}
+      preserveSelectOrder={true}
+      showOrderButtons={true}
+      options={options}
+      selected={selected}
+      onChange={(newValue) => setSelected(newValue)}
+    />
   );
-
-  function handleDragEnd(event) {
-    const { over } = event;
-
-    setParent(over ? over.id : null);
-  }
 };
-
-function Draggable({id}) {
-  const { attributes, isDragging, transform, setNodeRef, listeners } =
-    useDraggable({
-      id,
-    });
-
-  return (
-    <button
-      ref={setNodeRef}
-      style={{
-        transform: CSS.Translate.toString(transform),
-        boxShadow: isDragging
-          ? "-1px 0 15px 0 rgba(34, 33, 81, 0.01), 0px 15px 15px 0 rgba(34, 33, 81, 0.25)"
-          : undefined,
-      }}
-      {...attributes}
-      {...listeners}
-    >
-      Drag me
-    </button>
-  );
-}
-
-function Droppable({ id, children }) {
-  const { isOver, setNodeRef } = useDroppable({ id });
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        width: 150,
-        height: 150,
-        border: "1px solid",
-        margin: 20,
-        borderColor: isOver ? "#4c9ffe" : "#EEE",
-      }}
-      ref={setNodeRef}
-    >
-      {children}
-    </div>
-  );
-}
