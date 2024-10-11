10bfa8a8fa45724581342da6299c8a6ad274cf14
wfarinas <wgu70804@gmail.com>
2024-10-11 00:31:08 -0600

    drag

--- az204/src/pages/PreguntaChecks.jsx
+++ az204/src/pages/PreguntaChecks.jsx
@@ -1,123 +1,82 @@
-import React, { useCallback, useRef, useState } from "react";
-import { DndProvider } from "react-dnd";
-import HTML5Backend from "react-dnd-html5-backend";
+import * as React from "react";
+import { useMemo, useState } from "react";
+import * as ReactDOM from "react-dom";
 
-import { useDrag, useDrop } from "react-dnd";
-import update from "immutability-helper";
+import {
+  DndContext,
+  useDraggable,
+  useDroppable,
+} from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
 
-export const ItemTypes = {
-  CARD: "card",
-};
+export const PreguntaChecks = () => {
+  const containers = ["A", "B", "C"];
+  const [parent, setParent] = useState(null);
 
-const style = {
-  border: "1px dashed gray",
-  padding: "0.5rem 1rem",
-  marginBottom: ".5rem",
-  backgroundColor: "white",
-  cursor: "move",
-};
-const Card = ({ id, text, index, moveCard }) => {
-  const ref = useRef(null);
-  const [{ isOver }, drop] = useDrop({
-    accept: ItemTypes.CARD,
-    drop(item, monitor) {
-      if (!ref.current) {
-        return;
-      }
-      const dragIndex = item.index;
-      const hoverIndex = index;
-      // Don't replace items with themselves
-      if (dragIndex !== hoverIndex) {
-        moveCard(dragIndex, hoverIndex);
-      }
-    },
-    collect: (monitor) => ({
-      isOver: monitor.isOver(),
-    }),
-  });
-  const [, drag] = useDrag({
-    item: { type: ItemTypes.CARD, id, index },
-  });
-  drag(drop(ref));
+  const item = <Draggable id={ "draggable-item"} />;
+
   return (
-    <>
-      <div style={{ border: `1px solid ${isOver ? "blue" : "transparent"}` }} />
-      <div ref={ref} style={{ ...style }}>
-        {text}
-      </div>
-    </>
-  );
-};
+    <DndContext onDragEnd={handleDragEnd}>
+      {parent === null ? item : null}
 
-const Container = () => {
-  {
-    const [cards, setCards] = useState([
-      {
-        id: 1,
-        text: "Write a cool JS library",
-      },
-      {
-        id: 2,
-        text: "Make it generic enough",
-      },
-      {
-        id: 3,
-        text: "Write README",
-      },
-      {
-        id: 4,
-        text: "Create some examples",
-      },
-      {
-        id: 5,
-        text: "Spam in Twitter and IRC to promote it (note that this element is taller than the others)",
-      },
-      {
-        id: 6,
-        text: "???",
-      },
-      {
-        id: 7,
-        text: "PROFIT",
-      },
-    ]);
-    const moveCard = useCallback(
-      (dragIndex, hoverIndex) => {
-        const dragCard = cards[dragIndex];
-        setCards(
-          update(cards, {
-            $splice: [
-              [dragIndex, 1],
-              [hoverIndex, 0, dragCard],
-            ],
-          })
-        );
-      },
-      [cards]
-    );
-    const renderCard = (card, index) => {
-      return (
-        <Card
-          key={card.id}
-          index={index}
-          id={card.id}
-          text={card.text}
-          moveCard={moveCard}
-        />
-      );
-    };
-    return (
-      <>
-        <div style={style}>{cards.map((card, i) => renderCard(card, i))}</div>
-      </>
-    );
+      <div style={{ display: "flex" }}>
+        {containers.map((id) => (
+          <Droppable key={id} id={id}>
+            {parent === id ? item : "Drop here"}
+          </Droppable>
+        ))}
+      </div>
+    </DndContext>
+  );
+
+  function handleDragEnd(event) {
+    const { over } = event;
+
+    setParent(over ? over.id : null);
   }
 };
 
-export const PreguntaChecks = () => {
+function Draggable({id}) {
+  const { attributes, isDragging, transform, setNodeRef, listeners } =
+    useDraggable({
+      id,
+    });
+
   return (
-    <DndProvider backend={HTML5Backend}>
-      <Container />
-    </DndProvider>
+    <button
+      ref={setNodeRef}
+      style={{
+        transform: CSS.Translate.toString(transform),
+        boxShadow: isDragging
+          ? "-1px 0 15px 0 rgba(34, 33, 81, 0.01), 0px 15px 15px 0 rgba(34, 33, 81, 0.25)"
+          : undefined,
+      }}
+      {...attributes}
+      {...listeners}
+    >
+      Drag me
+    </button>
   );
-};
+}
+
+function Droppable({ id, children }) {
+  const { isOver, setNodeRef } = useDroppable({ id });
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: 150,
+        height: 150,
+        border: "1px solid",
+        margin: 20,
+        borderColor: isOver ? "#4c9ffe" : "#EEE",
+      }}
+      ref={setNodeRef}
+    >
+      {children}
+    </div>
+  );
+}
